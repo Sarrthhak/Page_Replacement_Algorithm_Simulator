70ffb2b5514c64bd1cb2fb1352343a6507d97ea7
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2025-03-24 22:37:24 +0530

    Update main.py
    changed something in main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ui import SimulatorUI
+from algorithms import fifo_algorithm
 
 def run_simulation(pages, frames, algorithm):
+    if algorithm == "FIFO":
+        return fifo_algorithm(pages, frames)
     return 0, []
 
 ui = SimulatorUI(run_simulation)
