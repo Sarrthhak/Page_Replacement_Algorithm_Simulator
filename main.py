842b6192a6fa590e3f1114e0d6fcea2384903430
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2025-03-24 22:40:13 +0530

    Update main.py
    completed implementing lru and optimal algorithms

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from ui import SimulatorUI
-from algorithms import fifo_algorithm
+from algorithms import fifo_algorithm, lru_algorithm, optimal_algorithm
 
 def run_simulation(pages, frames, algorithm):
     if algorithm == "FIFO":
         return fifo_algorithm(pages, frames)
+    elif algorithm == "LRU":
+        return lru_algorithm(pages, frames)
+    elif algorithm == "Optimal":
+        return optimal_algorithm(pages, frames)
     return 0, []
 
 ui = SimulatorUI(run_simulation)
