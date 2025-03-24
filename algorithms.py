c44bd8e6557d729b9b8f544c0f9166d3afd1fdaa
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2025-03-24 22:36:25 +0530

    Update algorithms.py
    Added FIFO algorithm

--- algorithms.py
+++ algorithms.py
@@ -1,1 +1,14 @@
-
+def fifo_algorithm(pages, frames):
+    memory = []
+    faults = 0
+    steps = []
+    for page in pages:
+        if page not in memory and len(memory) < frames:
+            memory.append(page)
+            faults += 1
+        elif page not in memory:
+            memory.pop(0)
+            memory.append(page)
+            faults += 1
+        steps.append(memory.copy())
+    return faults, steps
